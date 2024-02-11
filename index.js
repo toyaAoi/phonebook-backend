ebba98df7a854cb9b92564146400544c99a1a618
T0YA <toyaAoi@users.noreply.github.com>
2024-02-11 18:35:17 +0530

    uibuild

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
 morgan.token("body-content", function (req, res) {
   return JSON.stringify(req.body);
 });
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :body-content"
